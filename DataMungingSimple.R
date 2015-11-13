0531c7cb53744d6de515f7688ba4eb6149a8c66c
livermok <kyle.livermore@hotmail.com>
2015-11-12 17:56:16 -0800

    worked on plot options

--- DataMungingSimple.R
+++ DataMungingSimple.R
@@ -6,8 +6,10 @@ length(cheapDiamonds)
 #plot(cheapDiamonds$carat, cheapDiamonds$price)
 #hist(cheapDiamonds$carat, breaks = 50, xlab = "price", ylab = "count")
 
-dataPlot <- ggplot(diamonds, aes(x = price, fill = cut)) + geom_histogram()
-print(dataPlot)
+#dataPlot <- ggplot(diamonds, aes(x = price, fill = cut)) + geom_histogram(binwidth = 100)
+#print(dataPlot)
 
 #Want to work on color selection for density
-#smoothScatter(cheapDiamonds$carat, cheapDiamonds$price, nbin = 2000, colramp = colorRampPalette(c("blue", "red")))
+smoothScatter(cheapDiamonds$carat, cheapDiamonds$price, nbin = 1000, nrpoints = 0, transformation = function(x) x^.5, colramp = colorRampPalette(c("black", "lightblue", "palegreen", "yellow", "orange", "red")))
+#types <- factor(cheapDiamonds$cut)
+#plot(cheapDiamonds$carat, cheapDiamonds$price, pch=as.integer(types), xlab = "carat", ylab ="price")
